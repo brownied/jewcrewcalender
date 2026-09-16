5cdecdbc2c122b14e6389979efd6c578662cfbae
brownied <84053724+brownied@users.noreply.github.com>
2026-09-15 22:25:48 -0500

    small date fix

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const events = [
     },
 
     {
-        date: "2026-09-13",
+        date: "2026-09-8",
         name: "Rosh Hashanah",
         time: "All lunch periods",
         location: "Skyloft",
